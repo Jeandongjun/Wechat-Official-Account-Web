cc9522d8815dcfb6cfe55c1cb9ec54043e540185
Jeandongjun <160682652+Jeandongjun@users.noreply.github.com>
2024-06-03 18:02:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const oa_name = "张洪Heo";
-const oa_description = "分享设计与科技生活";
-const web_beian = "京ICP备19051325号-2";
+const oa_name = "Jean的小世界";
+const oa_description = "分享设计与科技";
+const web_beian = " ";
 
 function replaceText(elementId, newText) {
   var element = document.getElementById(elementId);
